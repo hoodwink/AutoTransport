b9dc11e79664032092ab32cae098bfff0feb44db
gzinger <gregory.zinger@idt.net>
2016-06-20 12:10:41 -0400

    minor

--- db/ForegnKeys.sql
+++ db/ForegnKeys.sql
@@ -1,3 +1,13 @@
 alter table PDP.dbo.SampleData
  add CONSTRAINT FK_SampleData_Country FOREIGN KEY (COUNTRY)     
     REFERENCES PDP.dbo.Country_LU (COUNTRY);
+/*
+alter table PDP.dbo.ResultsData
+ add CONSTRAINT FK_ResultsData_Tolerance FOREIGN KEY (PDP_YEAR, PESTCODE, COMMOD)     
+    REFERENCES PDP.dbo.Tolerance_LU (PDP_YEAR, PESTCODE, COMMOD);
+
+	select * from PDP.dbo.ResultsData rd where
+	rd.PESTCODE='024' AND rd.PDP_YEAR=2010 AND rd.COMMOD='WG'
+	select * from PDP.dbo.Tolerance_LU t where
+	 t.PESTCODE='024' AND t.PDP_YEAR=2010 AND t.COMMOD='WG'
+*/
